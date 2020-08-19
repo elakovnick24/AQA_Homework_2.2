2d71fbb470f4f9b76e084f1ff04aef237ad1cd11
Nikita Elakov <89816960858@mail.ru>
2020-08-19 21:20:14 +0300

    changa selector [name]

--- src/test/java/DeliveryCard.java
+++ src/test/java/DeliveryCard.java
@@ -1,25 +1,21 @@
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryCard {
 
     @Test
     void shoudDeliveryCard() {
         open("http://localhost:7777");
-        SelenideElement form = $("[method=post]");
-        form.$("[data-test-id=city] input").setValue("Санкт-Петербург");
-        form.$("[data-test-id=date] input").setValue("13.08.2020");
-        form.$("[data-test-id=name] input").setValue("Николай");
-        form.$("[data-test-id=phone] input").setValue("+79111112222");
-        form.$("[data-test-id=agreement]").click();
-        form.$$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=city] input").setValue("Санкт-Петербург");
+        $("[placeholder='Дата встречи']").setValue("13.08.2020");
+        $("[data-test-id=name] input").setValue("Николай");
+        $("[data-test-id=phone] input").setValue("+79111112222");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
 
         $("[data-test-id=notification]").find(withText("Успешно!")).waitUntil(visible, 15000);
     }
@@ -27,69 +23,39 @@ public class DeliveryCard {
     @Test
     void checkValidationcity() {
         open("http://localhost:7777");
-        SelenideElement form = $("[method=post]");
-        form.$("[data-test-id=city] input").setValue("Выборг");
-        form.$("[data-test-id=date] input").setValue("21.08.2020");
-        form.$("[data-test-id=name] input").setValue("Николай");
-        form.$("[data-test-id=phone] input").setValue("+79111112222");
-        form.$("[data-test-id=agreement]").click();
-        form.$$("button").find(exactText("Забронировать")).click();
-
-        $("[data-test-id=notification]").find(withText("Успешно!")).waitUntil(visible, 15000);
+        $("[data-test-id=city] input").setValue("Выборг");
+        $("[data-test-id=date] input");
+        $("[data-test-id=name] input").setValue("Николай");
+        $("[data-test-id=phone] input").setValue("+79111112222");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=city] .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна"));
     }
 
-    @Test
-    void checkValidationDate() {
-        open("http://localhost:7777");
-        SelenideElement form = $("[method=post]");
-        form.$("[data-test-id=city] input").setValue("Санкт-Петербург");
-        form.$("[data-test-id=date] input").setValue("20.08.2020");
-        form.$("[data-test-id=name] input").setValue("Николай");
-        form.$("[data-test-id=phone] input").setValue("+79111112222");
-        form.$("[data-test-id=agreement]").click();
-        form.$$("button").find(exactText("Забронировать")).click();
-
-        $("[data-test-id=notification]").find(withText("Успешно!")).waitUntil(visible, 15000);
-    }
-
-    @Test
-    void checkValidationDate1() {
-        open("http://localhost:7777");
-        SelenideElement form = $("[method=post]");
-        form.$("[data-test-id=city] input").setValue("Санкт-Петербург");
-        form.$("[data-test-id=date] input").setValue("22.08.2020");
-        form.$("[data-test-id=name] input").setValue("Николай");
-        form.$("[data-test-id=phone] input").setValue("+79111112222");
-        form.$("[data-test-id=agreement]").click();
-        form.$$("button").find(exactText("Забронировать")).click();
-
-        $("[data-test-id=notification]").find(withText("Успешно!")).waitUntil(visible, 15000);
-    }
     @Test
     void checkValidationPhone() {
         open("http://localhost:7777");
-        SelenideElement form = $("[method=post]");
-        form.$("[data-test-id=city] input").setValue("Санкт-Петербург");
-        form.$("[data-test-id=date] input").setValue("13.08.2020");
-        form.$("[data-test-id=name] input").setValue("Николай");
-        form.$("[data-test-id=phone] input").setValue("+791111122220000");
-        form.$("[data-test-id=agreement]").click();
-        form.$$("button").find(exactText("Забронировать")).click();
-
-        $("[data-test-id=notification]").find(withText("Успешно!")).waitUntil(visible, 15000);
+        $("[data-test-id=city] input").setValue("Санкт-Петербург");
+        $("[data-test-id=date] input").setValue("13.08.2020");
+        $("[data-test-id=name] input").setValue("Николай");
+        $("[data-test-id=phone] input").setValue("+791111122220000");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
+}
 
 /*    @Test
     void blockMenuCity() {
         open("http://localhost:7777");
-        SelenideElement form = $("[method=post]");
-        form.$("[data-test-id=city] input").setValue("Сан");
-        form.$(".menu.input__menu").find(String.valueOf(exactText("Санкт-Петербург"))).click();
-        form.$("[data-test-id=date] input").setValue("13.08.2020");
-        form.$("[data-test-id=name] input").setValue("Николай");
-        form.$("[data-test-id=phone] input").setValue("+791111122220000");
-        form.$("[data-test-id=agreement]").click();
-        form.$$("[tabindex=0]").find(exactText("Санкт-Петербург")).click();
+
+        $("[data-test-id=city] input").setValue("Сан");
+        $(".menu.input__menu").find(String.valueOf(exactText("Санкт-Петербург"))).click();
+        $("[data-test-id=date] input").setValue("13.08.2020");
+        $("[data-test-id=name] input").setValue("Николай");
+        $("[data-test-id=phone] input").setValue("+791111122220000");
+        $("[data-test-id=agreement]").click();
+        $$("[tabindex=0]").find(exactText("Санкт-Петербург")).click();
 
         $("[data-test-id=notification]").find(withText("Успешно!")).waitUntil(visible, 15000);
     }
